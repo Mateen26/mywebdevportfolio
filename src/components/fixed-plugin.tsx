4bd30c7773bc49a5f7f16923932f5b83ccd6674c
Mateen Rajput <165633730+muhammadMateen26@users.noreply.github.com>
2024-09-24 10:02:09 +0500

    fixes

--- src/components/fixed-plugin.tsx
+++ src/components/fixed-plugin.tsx
@@ -5,9 +5,8 @@ import { Button } from "@material-tailwind/react";
 export function FixedPlugin() {
   return (
     <a href="https://www.material-tailwind.com" target="_blank">
-      <Button
+      <button
         color="white"
-        size="sm"
         className="!fixed bottom-4 right-4 flex gap-1 pl-2 items-center border border-blue-gray-50"
       >
         <Image
@@ -18,7 +17,7 @@ export function FixedPlugin() {
           src="https://www.material-tailwind.com/favicon.png"
         />{" "}
         Made With Material Tailwind
-      </Button>
+      </button>
     </a>
   );
 }
