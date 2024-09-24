5256ba0b95264f81e51c073230627cffc76a05e9
Mateen Rajput <165633730+muhammadMateen26@users.noreply.github.com>
2024-09-24 09:27:26 +0500

    more fixes

--- src/app/contact-form.tsx
+++ src/app/contact-form.tsx
@@ -51,17 +51,7 @@ export function ContactForm() {
                   Open Support Ticket
                 </h6>
               </div>
-              <div className="flex items-center gap-5">
-                <IconButton variant="text" color="white">
-                  <i className="fa-brands fa-facebook text-lg" />
-                </IconButton>
-                <IconButton variant="text" color="white">
-                  <i className="fa-brands fa-instagram text-lg" />
-                </IconButton>
-                <IconButton variant="text" color="white">
-                  <i className="fa-brands fa-github text-lg" />
-                </IconButton>
-              </div>
+           
             </div>
             <div className="w-full mt-8 md:mt-0 md:px-10 col-span-4 h-full p-5">
               <form action="#">
