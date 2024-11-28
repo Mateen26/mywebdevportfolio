a8f0becf695e29e5bc8258a8cfcb752ab9883c63
Muhammad Mateen <mateenrajput55@gmail.com>
2024-11-28 12:31:20 +0500

    asd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const roboto = Roboto({
 export const metadata: Metadata = {
   title: "Next.js + Tailwind Portfolio Page",
   description:
-    "Introducing Tailwind Portfolio Page, a versatile and stylish portfolio template built on the foundation of Tailwind CSS and Material Tailwind.",
+    "Introducing Tailwind  Portfolio Page, a versatile and stylish portfolio template built on the foundation of Tailwind CSS and Material Tailwind.",
 };
 
 export default function RootLayout({
